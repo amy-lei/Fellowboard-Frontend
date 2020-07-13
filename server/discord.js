b12b0247f6cead81fbe3c3507da419755f31d4be
YaHeng Su <yaheng.su1@gmail.com>
2020-07-13 15:59:06 -0600

    get channels for post fetching

--- server/discord.js
+++ server/discord.js
@@ -5,23 +5,42 @@ require('dotenv').config();
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const DISCORD_GUILD = '716052909271285803';
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}`);
+    console.log('Discord - Getting guild/server');
     const guild = client.guilds.resolve(DISCORD_GUILD);
-    const members = guild.members.cache;
+    console.log('Discord - Got guild/server');
     console.log('Discord - Getting users');
-    getUsers(client);
+    getUsers(client, guild);
     console.log('Discord - Got users');
+    console.log('Discord - Fetching posts');
+    fetchPosts(client, guild);
+    console.log('Discord - fetched posts');
 });
 
 client.login(DISCORD_TOKEN);
 
-async function fetchPosts(client) {
-    
+function fetchPosts(client, guild) {
+    //id for topics category (where all the golang, python, etc. channels are)
+    const CATEGORY_ID = '716458296030265344';
+    //if we want to ignore channels like #battlestations, #coffee, etc.
+    const IGNORE_LIST = [];
+
+    const guildChannels = guild.channels.cache;
+    const channels = [];
+
+    guildChannels.each(channel => {
+        if(channel.parentID === CATEGORY_ID) {
+            channels.push({
+                "id": channel.id,
+                "name": channel.name
+            });
+        }
+    })
+    console.log(channels);
 };
 
-function getUsers(client) {
-    const guild = client.guilds.resolve(DISCORD_GUILD);
+function getUsers(client, guild) {
     const members = guild.members.cache;
     const users = [];
     members.each(member => {
