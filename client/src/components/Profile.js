752dee1a3628e02450f67e1254d24538442dc9c7
Amy <leiamy12@gmail.com>
2020-07-15 17:14:50 -0400

    Add functionality for editting discord username

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,51 @@
 import React, { useState } from 'react';
+import save from '../assets/save.svg';
+import edit from '../assets/edit.svg';
+import cancel from '../assets/cancel.svg';
 
 function Profile(props) {
+    const [ value, setValue ] = useState(props.discord || "");
+    const [ editting, setEditting ] = useState(false);
+
+    const discord = (
+        <span className='profile-discord'>
+            {editting 
+                ? (<>
+                    <input
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                        placeholder="Set discord username"
+                        className="profile-discord_input"
+                    />
+                    <img
+                        src={save}
+                        onClick={()=> {alert("saved discord username"); setEditting(false)}}
+                    />
+                    <img
+                        src={cancel}
+                        onClick={()=> setEditting(false)}
+                    />
+                    </>
+                ) : (
+                    <>
+                    <label
+                        value={value}
+                        className="profile-discord_input"
+                    >
+                        {props.discord || "Set discord username"}
+                    </label>
+                    <img
+                        src={edit}
+                        onClick={()=> setEditting(true)}
+                    />
+
+                    </>
+                )
+            }
+        </span>
+    );
+     
+
     return (
         <div className='profile'>
             <div className='profile-avatar'></div>
@@ -8,9 +53,7 @@ function Profile(props) {
                 <h2 className='profile-name'>
                     {props.fullname || props.username}
                 </h2>
-                <span className='profile-discord'>
-                    {props.discord || "Please enter your discord username"}
-                </span>
+                {discord}
             </section>  
         </div>
     )
