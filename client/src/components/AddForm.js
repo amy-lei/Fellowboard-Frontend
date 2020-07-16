de34366a317389f0e0c941ae7da17a1b1c1ec925
Amy <leiamy12@gmail.com>
2020-07-16 16:26:13 -0400

    Add basic post form

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,202 +1,221 @@
-import React,{useState, setIsOpen } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
-import "../styles/modal.scss";
-import Modal from 'react-modal';
-import { Controller, useForm } from "react-hook-form";
-import { TextArea , TextInput } from '@thumbtack/thumbprint-react';
-
-const customStyles = {
-  content : {
-    top                   : '30%',
-    left                  : '35%',
-    right                 : '5%',
-    bottom                : '8%',
-  }
-};
+import React, { useState } from 'react';
+import { toHexColor } from '../util';
+import { 
+  Button, 
+  Icon, 
+  Form,
+} from "semantic-ui-react";
 
 function AddForm() {
-  var subtitle;
-  const [GeneralIsOpen, setGeneralIsOpen] = React.useState(false);
-  const [GithubIsOpen, setGithubIsOpen] = React.useState(false);
-  const [GithubPRIsOpen, setGithubPRIsOpen] = React.useState(false);
-  const [value,setValue]=useState('');
-  const handleSelect=(e)=>{
-    console.log(e);
-    setValue(e)
+  const [ open, setOpen ] = useState(false);
+  const [ type, setType ] = useState('');
+  const [ content, setContent ] = useState({});
+  const [ isPublic, setIsPublic ] = useState(true);
+  const [ tags, setTags ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+
+  const submit = (e) => {
+    e.preventDefault();
+    switch(type) {
+      case 'text':
+        const body = {
+          type,
+          tags,
+          isPublic,
+          title: content.title,
+          content: {
+            description: content.description || '',
+            url: content.url || '',
+          },
+          creator: 'server', // change this to the creator's gh username
+        }
+        console.log(body);
+        setOpen(false);
+        setType('');
+        setContent({});
+        setIsPublic(true);
+        setTags([]);
+        break;
+      case 'youtube':
+      case 'github':
+        setIsLoading(true);
+        break;
+      default:
+        break;
+    }
+    // reset values
+
   }
 
-  /*
-  openModal function for 5 diferrent types of models
-  */
-  function openGeneralModal() {
-    setGeneralIsOpen(true);
-  }
-
-  function openGithubModal() {
-    setGithubIsOpen(true);
-  }
-
-  function openGithubPRModal() {
-    setGithubPRIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#2D2D2D';
-  }
-
-
-  /*
-  closeModal function for 5 diferrent types of models
-  */
-  function closeGeneralModal(){
-    setGeneralIsOpen(false);
-  }
-
-  function closeGithubModal(){
-    setGithubIsOpen(false);
-  }
-
-  function closeGithubPRModal(){
-    setGithubPRIsOpen(false);
-  }
-
-  const { handleSubmit, register, errors } = useForm();
-
-  /*
-  Define states for each modal's contents
-  */
-  const [textGeneralTitle, settextGeneralTitle] = React.useState('');
-  const [textGeneralBody, settextGeneralBody] = React.useState('');
-
-  const [textGithubTitle, settextGithubTitle] = React.useState('');
-  const [textGithubBody, settextGithubBody] = React.useState('');
-
-  const [textGithubPRTitle, settextGithubPRTitle] = React.useState('');
-  const [textGithubPRBody, settextGithubPRBody] = React.useState('');
-
-
-  const onSubmitGeneral = () => {
-    console.log(textGeneralTitle);
-  };
-
-  const onSubmitGithub= () => {
-    console.log(textGeneralTitle);
-  };
-
-  const onSubmitGithubPR = () => {
-    console.log(textGeneralTitle);
-  };
-
-
-  return (    
-    <div class="is-stuckToBottom">
-      <DropdownButton
-      title="+ Add Post"
-      key = 'up'
-      id="dropdown-menu-align-up"
-      drop = {'up'}
-      onSelect={handleSelect}
-        >
-              <Dropdown.Item onClick={openGeneralModal} eventKey="option-1">General</Dropdown.Item>
-              <Modal
-                isOpen={GeneralIsOpen}
-                onAfterOpen={afterOpenModal}
-                onRequestClose={closeGeneralModal}
-                style={customStyles}
-                contentLabel="General Post Form" >
-                <h2 ref={_subtitle => (subtitle = _subtitle)}>Add Post</h2>
-                {/* <form onSubmit={handleSubmit(onSubmit)}> */}
-                <form>
-                <label>Title of your post</label>
-                <TextInput
-                    size="small"
-                    value={textGeneralBody}
-                    placeholder="e.g. How to hack"
-                    onChange={v => settextGeneralBody(v)}
-                  
-                />
-
-                <label>Describe your post</label>
-                <TextArea
-                    placeholder="Post description"
-                    onChange={v => settextGeneralTitle(v)}
-                    value={textGeneralTitle}
-                />
-
-                {/* <button type="submit">Submit</button> */}
-                <button onClick={handleSubmit(onSubmitGeneral)}>Submit</button>
-                <button onClick={closeGeneralModal}>close</button>
-
-                </form>
-              </Modal>
-
-              <Dropdown.Item onClick={openGithubModal} eventKey="option-2">Github Issue</Dropdown.Item>
-              <Modal
-                isOpen={GithubIsOpen}
-                onAfterOpen={afterOpenModal}
-                onRequestClose={closeGithubModal}
-                style={customStyles}
-                contentLabel="Github Post Form" >
-                <h2 ref={_subtitle => (subtitle = _subtitle)}>Add Post</h2>
-                <form>
-
-                <label>Title of your post</label>
-                <TextInput
-                    size="small"
-                    value={textGithubTitle}
-                    placeholder="example@example.com"
-                    onChange={v => settextGithubTitle(v)}
-                />
-
-                <label>Describe your post</label>
-                <TextArea
-                    placeholder="Post description"
-                    onChange={v => settextGithubBody(v)}
-                    value={textGithubBody}
-                />
-                <button onClick={handleSubmit(onSubmitGithub)}>Submit</button>
-                <button onClick={closeGithubModal}>close</button>
-
-                </form>
-              </Modal>
-
-              <Dropdown.Item eventKey="option-3">Github PR</Dropdown.Item>
-              <Modal
-                isOpen={GithubPRIsOpen}
-                onAfterOpen={afterOpenModal}
-                onRequestClose={closeGithubPRModal}
-                style={customStyles}
-                contentLabel="Github Post Form" >
-                <h2 ref={_subtitle => (subtitle = _subtitle)}>Add Post</h2>
-                <form>
-
-                <label>Title of your post</label>
-                <TextInput
-                    size="small"
-                    value={textGithubPRTitle}
-                    placeholder="example@example.com"
-                    onChange={v => settextGithubPRTitle(v)}
-                />
-
-                <label>Describe your post</label>
-                <TextArea
-                    placeholder="Post description"
-                    onChange={v => settextGithubPRBody(v)}
-                    value={textGithubPRBody}
-                />
-                <button onClick={handleSubmit(onSubmitGithubPR)}>Submit</button>
-                <button onClick={closeGithubPRModal}>close</button>
-
-                </form>
-              </Modal>
-
-      </DropdownButton>
-
+  let formContent;
+  if (type === '') {
+    formContent = (
+      <div className='add-options'>
+          <Button 
+            icon
+            labelPosition='left'
+            className='add-options_choice'
+            onClick={() => setType('text')}
+          > 
+            <Icon name='sticky note'/>
+            Notes
+          </Button>
+          <Button 
+            icon
+            labelPosition='left'
+            className='add-options_choice'
+            onClick={() => setType('youtube')}
+          > 
+            <Icon name='youtube'/>
+            Youtube
+          </Button>
+          <Button 
+            icon
+            labelPosition='left'
+            className='add-options_choice'
+            onClick={() => setType('github')}
+          > 
+            <Icon name='github alternate'/>
+            Github
+          </Button>
       </div>
+    );
+  } else if (type === 'text'){
+      formContent = (
+        <Form className='add-text' onSubmit={submit}>
+          <h3>
+          <Icon name='sticky note'/>Post new note
+          </h3>            
+          <Form.Input
+            fluid
+            required
+            label="Title"
+            placeholder="e.g. Intro to MERN stack"
+            value={content.title}
+            onChange={(e, {value}) => setContent({ ...content, title: value })}
+          />
+          <Form.Input
+            icon='linkify'
+            iconPosition='left'
+            value={content.url}
+            onChange={(e, {value}) => setContent({ ...content, url: value })}
+            />  
+          <Form.TextArea
+            label='Description'
+            placeholder='e.g. Learn to build and deploy your first MERN project'
+            value={content.description}
+            onChange={(e, {value}) => setContent({ ...content, description: value })}
+          />
+          <TagForm tags={tags} setTags={setTags}/>
+          <Form.Group widths='equal'>
+            <Form.Radio
+              label='Public'
+              checked={isPublic}
+              onChange={(e, {checked}) => setIsPublic(checked)}
+            />
+            
+            <Form.Radio
+              label='Private'
+              checked={!isPublic}
+              onChange={(e, {checked}) => setIsPublic(!checked)}
+            />
+          </Form.Group>
+          <button className='add-form-submit'>
+            Create
+          </button>
+        </Form>
+      );
+    } else if (type === 'youtube' || type === 'github') {
+      const title = type === 'youtube' ? 'Post a Youtube Video' : 'Post from Github';
+      formContent = (
+        <Form className='add-text' onSubmit={submit}>
+          <h3><Icon name={type}/>{title}</h3>
+          {isLoading 
+            ? (<div className='loader'></div>)
+            :
+            (<>
+              <Form.Input
+                icon='linkify'
+                iconPosition='left'
+                onChange={(e, {value}) => setContent({...content, url: value})}
+                value={content.url || ''}
+              />
+              <TagForm tags={tags} setTags={setTags}/>
+              <button className='add-form-submit'>
+                Create
+              </button>
+            </>)
+        }
+        </Form>
+      );
+    } 
+
+  return ( 
+    <div>
+      <Button
+        circular
+        icon='plus'
+        size='big'
+        className={`add-btn ${open ? 'opened' : 'closed'}`}
+        onClick={() => { setOpen(!open); setType(''); }}
+      />
+      { open && (
+        <div 
+          className={`add-form ${open ? 'opened' : 'closed'} ${type}`}
+        >
+          {formContent}
+        </div>
+      )}
+    </div>
   );
+}
+
+function TagForm(props) {
+  const [ value, setValue ] = useState('');
+  
+  // remove tag at given index
+  const removeTag = (idx) => {
+    const tags = [...props.tags];
+    tags.splice(idx, 1);
+    props.setTags(tags);
+  }
+
+  // add tags split by commas
+  const addTag = (e, {value}) => {
+    if (value && value.charAt(value.length - 1) === ',') {
+      const tag = value.substring(0, value.length - 1);
+      props.setTags(props.tags.concat(tag));
+      setValue('');
+    } else {
+      setValue(value);
+    }
+  }
+
+  const tags = props.tags.map((tag, i) => {
+    return (
+      <span 
+        key={i}
+        className='tag' 
+        style={{backgroundColor: toHexColor(tag)}}
+        onClick={() => removeTag(i)}
+      >
+        {`#${tag}`}
+      </span>
+    )});
+
+  return(
+    <div className='tag-form'>
+      <div className='tag-form_container'>
+        {tags}
+      </div>
+      <Form.Input
+        label='Tags'
+        placeholder='Add a comma to confirm each tag'
+        value={value}
+        onChange={addTag}
+      />
+    </div>
+  )
 }
 
 export default AddForm;
