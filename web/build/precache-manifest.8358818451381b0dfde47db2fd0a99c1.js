ddaa1c2797de6bc4baaf0ce8dae3a0b292c10041
rch-liu <rachel98rl@hotmail.com>
2020-07-16 16:46:37 -0400

    Commit from heroku-deploy.py for deployment

---
+++ web/build/precache-manifest.8358818451381b0dfde47db2fd0a99c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a20303113f83a73f662e3ca51217442",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb17946c72b2828bb577",
+    "url": "/static/css/main.e5abfa2e.chunk.css"
+  },
+  {
+    "revision": "99b85a0f2bea698c0c43",
+    "url": "/static/js/2.3763e449.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3763e449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb17946c72b2828bb577",
+    "url": "/static/js/main.13469a27.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8ce1b2c1ca974b5e0ab6a9d9c834c156",
+    "url": "/static/media/pin-filled.8ce1b2c1.svg"
+  },
+  {
+    "revision": "7c3df1052b9326fcdd2de2057de976d4",
+    "url": "/static/media/pin-outline.7c3df105.svg"
+  },
+  {
+    "revision": "8d146a4a95219dc65746292cf74daf2a",
+    "url": "/static/media/search.8d146a4a.svg"
+  }
+]);
