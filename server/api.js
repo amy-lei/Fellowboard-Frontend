10f290b0a62c25da2625018071b2aab062898242
rch-liu <rachel98rl@hotmail.com>
2020-07-16 15:22:12 -0400

    Add heroku deploy

--- server/api.js
+++ server/api.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const fetch = require('node-fetch');
 const router = express.Router();
-// const tools = require('./github');
+const path = require('path')
 
-// tools.fetchUsers();
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+})
 
 module.exports = router;
 
