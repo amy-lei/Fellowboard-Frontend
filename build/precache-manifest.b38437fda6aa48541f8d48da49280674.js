7d3d866fbb612ef78106b755837a6dec75c28814
rch-liu <rachel98rl@hotmail.com>
2020-07-17 04:15:20 -0400

    Commit from heroku-deploy.py for deployment

---
+++ build/precache-manifest.b38437fda6aa48541f8d48da49280674.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b7c007722fefbf068c13134178ea98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "004dff1ad685628742d3",
+    "url": "/static/css/main.c9d68a16.chunk.css"
+  },
+  {
+    "revision": "103eaff74f8238c747a0",
+    "url": "/static/js/2.d78a35a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d78a35a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004dff1ad685628742d3",
+    "url": "/static/js/main.1cf20ad9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2040f22004520a6c4e3f7e43127ae633",
+    "url": "/static/media/cancel.2040f220.svg"
+  },
+  {
+    "revision": "b5e1292a8ba40238f53c33ae0542b3dd",
+    "url": "/static/media/edit.b5e1292a.svg"
+  },
+  {
+    "revision": "96e41f0bdf3fe8eb6933127f92cdde09",
+    "url": "/static/media/illustration.96e41f0b.svg"
+  },
+  {
+    "revision": "8ce1b2c1ca974b5e0ab6a9d9c834c156",
+    "url": "/static/media/pin-filled.8ce1b2c1.svg"
+  },
+  {
+    "revision": "7c3df1052b9326fcdd2de2057de976d4",
+    "url": "/static/media/pin-outline.7c3df105.svg"
+  },
+  {
+    "revision": "2f36ebaacae424e5c02224dbd5dd55b4",
+    "url": "/static/media/save.2f36ebaa.svg"
+  },
+  {
+    "revision": "8d146a4a95219dc65746292cf74daf2a",
+    "url": "/static/media/search.8d146a4a.svg"
+  }
+]);
---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  githubId: {
+    type: String,
+    required: true,
+  },
+  username: {
+    // GitHub username
+    type: String,
+    required: true,
+  },
+  avatarUrl: {
+    type: String,
+    required: true,
+  },
+  fullname: String,
+  discord: String,
+  pinnedPosts: { type: [mongoose.Types.ObjectId], default: [] },
+});
+
+// Exports the model
+module.exports = User = mongoose.model("user", UserSchema);
