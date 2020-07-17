c812703cfaae379b6882554b0c97f2e3f0c3bee6
Amy <leiamy12@gmail.com>
2020-07-17 04:56:23 -0400

    Commit from heroku-deploy.py for deployment

--- web/build/precache-manifest.1771a3b884bdae19997275d7127e78ad.js
+++ web/build/precache-manifest.1771a3b884bdae19997275d7127e78ad.js
@@ -1,6 +1,5 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-<<<<<<< HEAD:web/build/precache-manifest.1771a3b884bdae19997275d7127e78ad.js
     "revision": "b6bea999b36b0ee0eff2127e0a0f1803",
     "url": "/index.html"
   },
@@ -19,26 +18,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "208a4d63eb3778d84346",
     "url": "/static/js/main.b7a66d0a.chunk.js"
-=======
-    "revision": "5487f865dba16eaa6df6ff47b276409f",
-    "url": "/index.html"
-  },
-  {
-    "revision": "cb61e54f4170d5ec272a",
-    "url": "/static/css/main.05654db9.chunk.css"
-  },
-  {
-    "revision": "1326eda9ce9811ca343a",
-    "url": "/static/js/2.1c91149b.chunk.js"
-  },
-  {
-    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
-    "url": "/static/js/2.1c91149b.chunk.js.LICENSE.txt"
-  },
-  {
-    "revision": "cb61e54f4170d5ec272a",
-    "url": "/static/js/main.aeebdf19.chunk.js"
->>>>>>> b18b01c95d321cc7b9e55a2f5fc757465b490d68:web/build/precache-manifest.334adb5c7ded0abe96d708b60a9eba44.js
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
