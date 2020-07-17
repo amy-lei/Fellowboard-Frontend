cf1f997a0cb12b9181b0d3880b30769097b52e00
Amy <leiamy12@gmail.com>
2020-07-17 11:02:37 -0400

    Style navbar

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -3,14 +3,17 @@ import search from "../assets/search.svg";
 import { AuthContext } from "../App";
 function SearchBar(props) {
   const scrollRef = useRef(null);
+  const menuRef = useRef(null);
   const [inputValue, setInputValue] = useState("");
 
   const handleScroll = () => {
     const position = window.pageYOffset;
     if (position < 300) {
       scrollRef.current.className = "search";
+      menuRef.current.className = "menu-list";
     } else if (position >= 100) {
       scrollRef.current.className = "search fixed";
+      menuRef.current.className = "menu-list fixed";
     }
   };
 
@@ -24,7 +27,6 @@ function SearchBar(props) {
 
   const { state, dispatch } = useContext(AuthContext);
   const handleOnChange = (e) => {
-    // TODO: perhaps break at new tags? actually use query?
     setInputValue(e.target.value);
   };
 
@@ -37,7 +39,7 @@ function SearchBar(props) {
   };
 
   return (
-    <section>
+    <section className='nav'>
       <div ref={scrollRef} className="search">
         <input
           value={inputValue}
@@ -51,21 +53,21 @@ function SearchBar(props) {
           onClick={() => props.setFilter(inputValue)}
         />
       </div>
-      <ul className="menu-list">
+      <ul ref={menuRef} className="menu-list">
         <li
-          className={selectedFilter === "explore" ? "" : "not-selected"}
+          className={selectedFilter === "explore" ? "selected" : "not-selected"}
           onClick={() => changeSelectedFilter("explore")}
         >
           Explore
         </li>
         <li
-          className={selectedFilter === "dashboard" ? "" : "not-selected"}
+          className={selectedFilter === "dashboard" ? "selected" : "not-selected"}
           onClick={() => changeSelectedFilter("dashboard")}
         >
           Dashboard
         </li>
         <li
-          className={selectedFilter === "contacts" ? "" : "not-selected"}
+          className={selectedFilter === "contacts" ? "selected" : "not-selected"}
           onClick={() => changeSelectedFilter("contacts")}
         >
           Contacts
