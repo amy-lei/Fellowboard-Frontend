ab8ceab07ec60b08c6f5e580d941c071b62679bc
rch-liu <rachel98rl@hotmail.com>
2020-07-17 03:11:25 -0400

    Revert "remove api.js"
    This reverts commit 3e996b31f552276c1d7ccb5af2a65c83287f32c3.

---
+++ server/api.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const fetch = require('node-fetch');
+const router = express.Router();
+// const tools = require('./github');
+
+// tools.fetchUsers();
+
+module.exports = router;
+
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const User = require("./models/User");
 const Post = require("./models/Post");
 const postRouter = require("./routes/posts.js");
 const userRouter = require("./routes/user.js");
+const api = require("./api.js");
 require("dotenv").config();
 
 // connect to database
@@ -31,6 +32,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: "text/*" }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api", api);
 app.use("/api/posts", postRouter);
 app.use("/api/users", userRouter);
 app.use((req, res, next) => {
