80f408105e59a3fd7b0644dcef4f32f8e2a50883
rch-liu <rachel98rl@hotmail.com>
2020-07-17 02:59:52 -0400

    Commit from heroku-deploy.py for deployment

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 });
 
 if (process.env.ENV === "PRODUCTION") {
-  const reactPath = path.resolve(__dirname, "..", "client", "build");
+  const reactPath = path.resolve(__dirname, "build");
   app.use(express.static(reactPath));
   app.get("*", (req, res) => {
     res.sendFile(path.join(reactPath, "index.html"));
